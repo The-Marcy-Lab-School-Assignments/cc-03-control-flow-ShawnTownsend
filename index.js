714d544054a9f7c1f3211fad79df835dc56cf3e9
ShawnTownsend <stitans28@gmail.com>
2021-10-04 14:03:12 +0000

    Completed questions 1-4

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
 //1
-function countToTen(){
-  
+function countFromOne(n){
+    for(var i = 1; i <= n; i++) {
+     console.log(i);
+}
 }
 
 //2
-function countFromOne(){
-  
+function countEveryOdd(n){
+     for(let i = 0; i <= n; i+=3){
+    if(i > 1){
+     console.log(i)
+}
 }
 
 //3
-function countEveryEven(){
-  
+function isNegative(num) { 
+    if (num < 0) {
+    return true;
+    } else
+    return false;
 }
 
 //4
-function countEveryOdd(){
-  
+function betweenFiveAndTwenty(num){
+     if(num >= 5 && num <= 20){
+     return true
+     }
+     else return false
 }
 
-//5 
-function countEvens(){
-  
-}
+//5
+function isAllLowerCase(){
 
